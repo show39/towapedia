2beb9bbdba2401c98f34e777bf6f686590471243
show39 <acacia.show@gmail.com>
2017-06-22 20:26:44 +0900

    create meaning post function

--- app/controllers/meanings_controller.rb
+++ app/controllers/meanings_controller.rb
@@ -1,2 +1,19 @@
 class MeaningsController < ApplicationController
+
+  def create
+    @meaning = Meaning.create(meaning_params)
+    @towatop = Towa.find(@meaning.towa.id)
+    @user = current_user
+    if @meaning.save
+      redirect_to towa_path(@meaning.towa)
+    else
+      flash.now[:alert] = @meaning.errors.full_messages.join(",")
+      render template: "towas/show"
+    end
+  end
+
+  private
+    def  meaning_params
+      params.require(:meaning).permit(:meaning).merge(towa_id: params[:towa_id], user_id: current_user.id)
+    end
 end
--- app/controllers/towas_controller.rb
+++ app/controllers/towas_controller.rb
@@ -2,8 +2,9 @@ class TowasController < ApplicationController
   before_action :authenticate_user!, only: :create
 
   def create
-    @meanings = Meaning.order("created_at DESC").includes(:towa)
+    @meanings = Meaning.order("created_at DESC").includes(:towa, :user)
     @towa = Towa.new(towa_params)
+    @user = current_user
     if @towa.save
       redirect_to root_path, notice: '『とは』がまたひとつ作成されました'
     else
@@ -14,9 +15,10 @@ class TowasController < ApplicationController
 
   def show
     @towatop = Towa.find(params[:id])
-    @user = current_user
-    @towa = Towa.new
-    @towa.meanings.build(user_id: current_user.id)
+    if user_signed_in?
+      @user = current_user
+      @meaning = Meaning.new
+    end
   end
 
   private
