b0b56104c611ac80d265b36261ef2f11a136647c
show39 <acacia.show@gmail.com>
2017-06-24 14:58:37 +0900

    fix

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,13 +1,14 @@
 app_path = File.expand_path('../../../', __FILE__)
 
-worker_processes 1
+# worker_processes 1
+worker_processes 2
 
 working_directory "#{app_path}/current"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
-listen 3000
+# listen 3000
 timeout 60
 
 preload_app true
