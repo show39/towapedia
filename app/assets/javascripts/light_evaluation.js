69218d2637b4d5f825a3752b58ad1577c32149cc
show39 <acacia.show@gmail.com>
2017-07-19 14:25:13 +0900

    fix

--- app/assets/javascripts/light_evaluation.js
+++ app/assets/javascripts/light_evaluation.js
@@ -1,6 +1,8 @@
 $(document).on('turbolinks:load', function() {
   $(".light-group1,.light-group2,.light-group3").hover(function() {
-    $(".light-group1,.light-group2,.light-group3").attr("src", "/assets/light-off-4c4e686b109af4cc020a7fe84f115682cff8986868a5cb955af4fb9662248ac2.jpg");
+    $(".light-group1").attr("src", "/assets/light-off1-41763fca86985ad1647f3b40629e2129c1af8a380781948e7921b986dd843de7.jpg");
+    $(".light-group2").attr("src", "/assets/light-off2-1c1235a8a614862e63b1016ae9ada657e9ca4aa0fec800e46d4fd5a3fb16ef6c.jpg");
+    $(".light-group3").attr("src", "/assets/light-off3-051a7c213d3753d088f92229a2b70e577c92cbae645b626fbba80d77afdc79ec.jpg");
     $(this).attr("src", "/assets/light-on-7f6434b6f5408736ab9c65b380002e18ffb64246ad995e5ef1d7e79de4ee0d46.jpg");
     $(this).parents('form').prevAll().find('img').attr("src", "/assets/light-on-7f6434b6f5408736ab9c65b380002e18ffb64246ad995e5ef1d7e79de4ee0d46.jpg");
   });
